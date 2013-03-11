f794f1ec5def9784380ef1bee4fd56a98b05ed46
Kay Strobach <github@kay-strobach.de>
2013-03-11 20:34:13 +0100

    TER Release

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "intelli_cache".
-#
-# Auto generated 16-04-2012 10:51
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "intelli_cache".
+ *
+ * Auto generated 11-03-2013 19:42
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Intelligent Cache Management',
 	'description' => 'Auto clears cache based on extension names.',
 	'category' => 'be',
@@ -29,16 +29,22 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
-	'constraints' => array(
-		'depends' => array(
+	'version' => '0.0.2',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '4.5.0-6.0.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
-	'suggests' => array(
+	'suggests' => 
+	array (
 	),
 	'_md5_values_when_last_written' => 'a:3:{s:17:"ext_localconf.php";s:4:"0f09";s:14:"ext_tables.php";s:4:"d41d";s:31:"Classes/Hook/ProcessDatamap.php";s:4:"1670";}',
 );
